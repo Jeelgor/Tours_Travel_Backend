9c2a0667699f92bb09283eae8d4a41766ab18fec
Jeelgor <jeelgor10@gmail.com>
2024-12-02 22:45:09 +0530

    / add

--- Index.js
+++ Index.js
@@ -10,7 +10,7 @@ app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tours-travel-tau.vercel.app"],
+    origin: ["http://localhost:5173", "https://tours-travel-tau.vercel.app/"],
     headers: ["Content-Type"],
     methods: ["POST", "GET", "DELETE", "PUT", "PATCH", "OPTIONS"],
     credentials: true,
@@ -18,7 +18,7 @@ app.use(
 );
 app.options('*', cors())
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://tours-travel-tau.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://tours-travel-tau.vercel.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
