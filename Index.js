f20b36df9cc14e6863d925d4486d1bf256ccff05
Jeelgor <jeelgor10@gmail.com>
2024-12-15 10:53:13 +0530

    Update indexjs

--- Index.js
+++ Index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
-
+app.options("*", cors());
 // Routes
 app.use("/Auth/users", userRoutes);
 app.use("/api/payment", paymentRoutes);
