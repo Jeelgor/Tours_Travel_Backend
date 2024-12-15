f7a80e34eadc85a1e5683bb2d185e7c8d9683b0f
Jeelgor <jeelgor10@gmail.com>
2024-12-15 10:41:58 +0530

    allow origin

--- Index.js
+++ Index.js
@@ -13,7 +13,7 @@ connectMongoose();
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://tours-travel-tau.vercel.app/");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
